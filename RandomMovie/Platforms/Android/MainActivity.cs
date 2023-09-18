f89e1704c5f3ad87c87bffd84c08d1146ade92a5
Alexander Siegel <bindemaja80@googlemail.com>
2023-09-18 23:16:42 +0200

    Force Portrait

--- RandomMovie/Platforms/Android/MainActivity.cs
+++ RandomMovie/Platforms/Android/MainActivity.cs
@@ -7,4 +7,12 @@ namespace RandomMovie;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+
+        base.OnCreate(savedInstanceState);
+
+        RequestedOrientation = ScreenOrientation.Portrait;
+
+    }
 }
